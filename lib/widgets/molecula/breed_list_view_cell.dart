3df6c621618f1bfe73d79f376fff74898b2854c6
andrew <andrewdobrelya@gmail.com>
2023-01-31 14:41:22 +0200

    Assets generator has been added

--- lib/widgets/molecula/breed_list_view_cell.dart
+++ lib/widgets/molecula/breed_list_view_cell.dart
@@ -1,5 +1,7 @@
 import 'package:arch_sample/features/domain/entities/breed_info.dart';
 import 'package:arch_sample/features/presentation/breeds/bloc/breeds_bloc.dart';
+import 'package:arch_sample/generated/assets/assets.gen.dart';
+import 'package:arch_sample/generated/assets/fonts.gen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -12,7 +14,18 @@ class BreedListViewCellWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
-        title: Text(breed.nameFormatter),
+        title: Row(
+          children: [
+            Assets.images.icons.dogIcon.svg(width: 15, height: 15),
+            const SizedBox(
+              width: 15,
+            ),
+            Text(
+              breed.nameFormatter,
+              style: const TextStyle(fontFamily: FontFamily.roboto),
+            )
+          ],
+        ),
         onTap: () => context.read<BreedsBloc>().add(BreedsEvent.showInfo(breedInfo: breed)),
       ),
     );
